a62379f3346f7ef8dd29db0c3a3a92e70506a9a7
Jerry MacPherson <jmpherso@gmail.com>
2023-01-25 10:12:11 -0600

    Basic REACT/Webpack/Babel repo

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+//IMPORTING SCSS STYLE:
+import "./index.scss";
+//IMPORTING IMAGES:
+//import hello from "./images/helloworld.gif";
+//USING IMAGES:
+//<img src={hello} alt='hello world animated' />
+
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      // new rule to allow import of custom fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  // [devtool] this is an additional source map that will let the browser know what files are running our code.
+  // Helps with error tracing. Without it we will not know where our errors are coming from because it will state that everything inside the bundle file.
+  devtool: "eval-cheap-module-source-map",
+  // [devServer] configuration for the live server including port
+  devServer: {
+    // [static] config for how what to serve
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    // [port] what port on our local machine to run the dev server
+    port: 3000,
+  }
+}
