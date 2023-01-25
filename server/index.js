dce7c2e389172ddd65e04264eeb0a44015ed494e
eatthischair <based.god.swag.od@gmail.com>
2023-01-25 15:06:30 -0500

    actually added 'aaaa'

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express();
 // Todo insert app-wide middleware here when needed
 // app.use(express.static(path.join(__dirname, '')));
 // app.use(express.json());
-
+//aaaaaa
 // todo: Define routes here
 
 app.listen(process.env.PORT);
-console.log(`Server listening at http://localhost:${process.env.PORT}`)
+console.log(`Server listening at aaaa http://localhost:${process.env.PORT}`)
