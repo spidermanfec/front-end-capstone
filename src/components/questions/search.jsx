e52d5181ca70c612934a220d4f949f7e2b9f23ca
Jerry MacPherson <jmpherso@gmail.com>
2023-01-27 14:43:26 -0600

    Total basic layout finished, searchbar value tracked.

--- src/components/questions/search.jsx
+++ src/components/questions/search.jsx
@@ -3,13 +3,12 @@ import React, { useState } from 'react';
 function Search({ questionList, setQuestionList }) {
   const [searchTerm, setSearchTerm] = useState('');
   const onChangeSearch = (e) => {
-    console.log(e)
+    console.log(e.target.value);
   };
 
   return (
     <div className="search">
-      <input className="searchTerm" defaultValue="Have a question? Search for answers…" />
-      <button type="submit" className="searchButton" onChange={onChangeSearch}>SEARCH</button>
+      <input className="searchTerm" onChange={onChangeSearch} placeholder="Have a question? Search for answers…" />
     </div>
   );
 }
