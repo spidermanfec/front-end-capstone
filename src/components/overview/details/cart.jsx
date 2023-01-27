1e7f0ebbf0d13d3b192e44ce5acdd8937444865c
Robin Kim <eljjang@gmail.com>
2023-01-27 14:57:51 -0600

    minor css changes for cart feature

--- src/components/overview/details/cart.jsx
+++ src/components/overview/details/cart.jsx
@@ -3,12 +3,10 @@ import axios from 'axios';
 
 function Cart({ styles }) {
   const [open, setOpen] = React.useState(false);
-
   const [sizeSelected, setSizeSelected] = React.useState(false);
-
   const [sku, setSku] = React.useState('');
   const [size, setSize] = React.useState('');
-  const [quantity, setQuantity] = React.useState([]);
+  const [amount, setAmount] = React.useState('');
 
   const infos = Object.keys(styles.skus);
 
@@ -21,8 +19,11 @@ function Cart({ styles }) {
     e.preventDefault();
     let size = e.target.value;
     setSku(styles.skus[size])
+    setSize(styles.skus[size].size)
     setSizeSelected(true);
   };
+
+  console.log(size);
 
   const handleSelectQuantity = () => {
     let items = [];
@@ -33,22 +34,21 @@ function Cart({ styles }) {
   };
 
   // handle submitting to cart here
-  // const handleSubmiit = (e) => {
-  //   e.preventDefault();
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(size)
+  };
 
   return (
     <form>
       <select name="sizeList" id="sizeList" onClick={(e) => handleOpen(e)} onChange={handleChangeSize}>
         <option value="" disabled selected>Select Size</option>
-      {infos.map((info) => <option value={info}>{styles.skus[info].size}</option>)}
+        {infos.map((info) => <option value={info}>{styles.skus[info].size}</option>)}
       </select>
       <select name="quantityList" id="quantityList" onClick={sizeSelected}>
-      {(sizeSelected) ? handleSelectQuantity() : <option value="" disabled selected>--</option>}
+        {(sizeSelected) ? handleSelectQuantity() : <option value="" disabled selected>--</option>}
       </select>
-      <input type="submit" value="Add to Cart" onClick={((e) => {
-        handleSubmiit(e);
-      })}/>
+      <input type="submit" onClick={((e) => {handleSubmit(e)})} />
     </form>
   );
 }
