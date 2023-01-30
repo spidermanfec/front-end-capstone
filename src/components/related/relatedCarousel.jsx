d7de744dd797d58ef2a4bf929971b55321bed6fb
Jesus Solorzano <jesus.solorz@gmail.com>
2023-01-30 15:42:31 -0500

    rerendering on productID

--- src/components/related/relatedCarousel.jsx
+++ src/components/related/relatedCarousel.jsx
@@ -3,23 +3,25 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import Card from './card.jsx';
 
-export default function RelatedCarousel({ productID }) {
+export default function RelatedCarousel({ productID, setProduct }) {
   const [relatedProducts, setRelatedProducts] = useState([]);
 
   useEffect(() => {
     axios.get(`/products/${productID}/related`)
       .then((results) => setRelatedProducts(results.data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [productID]);
 
   return (
     <div className="card-carousel related-products">
       {relatedProducts.map((product) => (
         <Card
           listType="related"
+          id={`${product.id}`}
           category={product.category}
           name={product.name}
           defaultPrice={product.default_price}
+          setProduct={setProduct}
         />
       ))}
     </div>
@@ -28,4 +30,5 @@ export default function RelatedCarousel({ productID }) {
 
 RelatedCarousel.propTypes = {
   productID: PropTypes.string.isRequired,
+  setProduct: PropTypes.func.isRequired,
 };
