649609c8ba892ea0ec996c61d5e91000068d6f80
Jesus Solorzano <jesus.solorz@gmail.com>
2023-01-31 05:53:53 -0500

    renamed file, prev: card but without cond render

--- src/components/related/outfitCard.jsx
+++ src/components/related/outfitCard.jsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function OutfitCard({ productID, setOutfitProductsIDs, notInList }) {
-  const onAdd = () => {
-    if (notInList(productID)) {
-      setOutfitProductsIDs((oldIDs) => [...oldIDs, productID]);
-    }
-  };
-
+export default function Card({
+  id, category, name, defaultPrice, setProduct, setOutfitProductsIDs,
+}) {
   return (
     <div
-      className="card outfit-card"
-      onClick={onAdd}
-      onKeyPress={onAdd}
+      className="card"
+      onClick={() => setProduct(id)}
+      onKeyPress={() => setProduct(id)}
       role="button"
       tabIndex="0"
     >
-      +
+      <img className="card-img" src="https://i.ytimg.com/vi/_DEgejL9ap4/maxresdefault.jpg" alt=":(" />
+      <button
+        className="card-btn rm-outfit-btn"
+        type="button"
+        onClick={(e) => {
+          e.stopPropagation();
+          setOutfitProductsIDs(id);
+        }}
+      >
+        x
+      </button>
+      <div className="card-body">
+        <p>{category}</p>
+        <p>{name}</p>
+        <p>{defaultPrice}</p>
+        <p>rating</p>
+      </div>
     </div>
   );
 }
 
-OutfitCard.propTypes = {
-  productID: PropTypes.string.isRequired,
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  defaultPrice: PropTypes.string.isRequired,
+  setProduct: PropTypes.func.isRequired,
   setOutfitProductsIDs: PropTypes.func.isRequired,
-  notInList: PropTypes.func.isRequired,
 };
