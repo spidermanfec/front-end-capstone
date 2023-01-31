33ce6aa2c868528afe120df9b2f51fca57c512d3
Jesus Solorzano <jesus.solorz@gmail.com>
2023-01-31 06:00:05 -0500

    handles product removal by ID using hooks

--- src/components/related/outfitCarousel.jsx
+++ src/components/related/outfitCarousel.jsx
@@ -1,21 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Card from './card.jsx';
+import axios from 'axios';
+import PlusCard from './plusCard.jsx';
 import OutfitCard from './outfitCard.jsx';
 
-export default function OutfitCarousel({ productID }) {
+export default function OutfitCarousel({ productID, setProduct }) {
+  const [outfitProductsIDs, setOutfitProductsIDs] = useState([]);
   const [outfitProducts, setOutfitProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/outfit-products/', {
+      params: {
+        q: JSON.stringify(outfitProductsIDs),
+      },
+    })
+      .then((results) => results.data)
+      .then((results) => setOutfitProducts(results))
+      .catch((err) => console.log(err));
+  }, [outfitProductsIDs]);
+
+  const notInList = (id) => (
+    outfitProductsIDs.indexOf(id)
+  );
+
+  const removeProduct = (id) => {
+    setOutfitProductsIDs((oldIDs) => oldIDs.filter((oID) => oID !== id));
+  };
 
   return (
     <div className="card-carousel outfit-products">
-      <OutfitCard productID={productID} setOutfitProducts={setOutfitProducts} />
+      <PlusCard
+        productID={productID}
+        setOutfitProductsIDs={setOutfitProductsIDs}
+        notInList={notInList}
+      />
       {outfitProducts.map((productInfo) => (
-        <Card
-          listType="outfit"
+        <OutfitCard
+          id={`${productInfo.id}`}
           category={productInfo.category}
           name={productInfo.name}
           defaultPrice={productInfo.default_price}
-          onAction={setOutfitProducts}
+          setOutfitProductsIDs={removeProduct}
+          setProduct={setProduct}
         />
       ))}
     </div>
@@ -24,4 +50,5 @@ export default function OutfitCarousel({ productID }) {
 
 OutfitCarousel.propTypes = {
   productID: PropTypes.string.isRequired,
+  setProduct: PropTypes.func.isRequired,
 };
