4519f03a30a5e304cbd4de4c6cbdb2d967a801d0
Jesus Solorzano <jesus.solorz@gmail.com>
2023-02-06 19:12:44 -0500

    card image -> background image, styling

--- src/components/related/outfitCard.jsx
+++ src/components/related/outfitCard.jsx
@@ -28,7 +28,8 @@ export default function OutfitCard({
       role="button"
       tabIndex="0"
     >
-      <img className="card-img" src={photo} alt=":(" />
+      <div className="card-img" style={{backgroundImage: `url('${photo}')`}} />
+      {/* <img className="card-img" src={photo} alt=":(" /> */}
       <i
         className="fa-regular fa-circle-xmark card-btn rm-outfit-btn"
         role="button"
