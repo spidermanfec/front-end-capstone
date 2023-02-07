2e48054b5a2a25c8d29b67deab112f8e1d774743
Robin Kim <eljjang@gmail.com>
2023-02-06 18:29:03 -0600

    this looks so much better main image view filled

--- src/components/overview/details/selector.jsx
+++ src/components/overview/details/selector.jsx
@@ -27,6 +27,7 @@ function Selector({ itemStyles, handleStyleSelect, styles, selectedStyle, produc
       <p className="selectorGrid">
         {itemStyles.results?.map((itemStyle, index) => {
           console.log(itemStyle, index);
+          console.log('thumb url', itemStyle.photos[currentThumbnail].thumbnail_url, 'curr:', itemStyle.photos[index].thumbnail_url)
           return <button className={itemStyle.photos[currentThumbnail].thumbnail_url === itemStyle.photos[index].thumbnail_url ? 'selecStyleon' : 'selecStyleoff '} onClick={() => setCurrentThumbnail(itemStyle.photos.indexOf(itemStyle.photos[index]))} onClick={(e) => handleStyleSelect(e)}
             type="submit" value={itemStyle.style_id} key={index}>
             <div className='layover'>
